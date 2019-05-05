5a3506ae1679f50d64e98fe8ac7e9e078a2bf56f
david dai <david.xg.dai@gmail.com>
2019-05-05 21:32:30 +1000

    make android working.

--- android/app/src/main/java/com/whatdouc_native/MainApplication.java
+++ android/app/src/main/java/com/whatdouc_native/MainApplication.java
@@ -4,8 +4,11 @@ import android.app.Application;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -13,12 +16,16 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
   private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -29,19 +36,17 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new RNGestureHandlerPackage(),
             new FBSDKPackage(mCallbackManager),
-            new PickerPackage()
+            new PickerPackage(),
+            new MapsPackage()
       );
     }
 
     @Override
     protected String getJSMainModuleName() {
       return "index";
-    }
-
-    protected static CallbackManager getCallbackManager() {
-      return mCallbackManager;
     }
   };
 
@@ -53,6 +58,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    FacebookSdk.sdkInitialize(getApplicationContext());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export const setItem = (key, item) => {
   if (typeof item === 'object') {
@@ -7,10 +7,11 @@ export const setItem = (key, item) => {
   return AsyncStorage.setItem(key, item);
 };
 
-export const getItem = (key) => AsyncStorage.getItem(key).then(item => {
-  try {
-    return JSON.parse(item);
-  } catch (error) {
-    return item;
-  }
-});
+export const getItem = key =>
+  AsyncStorage.getItem(key).then(item => {
+    try {
+      return JSON.parse(item);
+    } catch (error) {
+      return item;
+    }
+  });
